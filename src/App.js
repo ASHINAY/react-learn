fd56f42df85619660c4bb2728503069a0a24a233
ASHINAY <anujamtech@gmail.com>
2023-12-19 21:24:31 +0530

    create player blocks

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
-import './App.css';
-// import nature from ".../assets/nature";
-import { quotes } from './constant';
+import "./App.css";
+import { buttonNames } from "./constant";
 
 function App() {
- 
+  const onDesktopBtnClick = (value, color) => {
+    console.log(value, color);
+    if (value === "Button1") {
+      console.log(`You  clicked the  ${color} color button`);
+    } else if (value === "Button2") {
+      console.log(`You  clicked the  ${color} color button`);
+    } else {
+      console.log(`You  clicked the  ${color} color button`);
+    }
+  };
+  const onMobileBtnClick = () => {
+    console.log("you clicked the mobile button");
+  };
+
   return (
-    <div className='root'>
-      <button className='desktopView'>Desktop</button>
-      <button className='mobileView'>MobileView</button>
-      {/* <button onClick={ColorChange} className="button">Change Color</button> */}
-      {/* <h1>{quotes}</h1> */}
+    <div className="root">
+      <button
+        className="desktopView"
+        onClick={() => onDesktopBtnClick("Button1", "green")}
+      >
+        {buttonNames[0]}
+      </button>
+      <button
+        className="desktopView1"
+        onClick={() => onDesktopBtnClick("Button2", "pink")}
+      >
+        {buttonNames[1]}
+      </button>
+      <button
+        className="desktopView1"
+        onClick={() => onDesktopBtnClick("Btn3", "black")}
+      >
+        {buttonNames[2]}
+      </button>
+      <button className="mobileView" onClick={onMobileBtnClick}>
+        MobileView
+      </button>
     </div>
   );
 }
